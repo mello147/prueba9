0f36043c24091e51321ff75b4bccf7a3a2e0e294
mello147 <jaidabel@hotmail.com>
2015-04-29 01:50:59 -0500

    yudeuiol

---
+++ www/js/index.js
@@ -0,0 +1,37 @@
+
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicity call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+
+        var ref = window.open('http://evaco.co', '_blank','location=no');
+        ref.addEventListener('loadstart', function(event) { console.log(event.type + ' - ' + event.url); } );
+        ref.addEventListener('loadstop', function(event) { console.log(event.type + ' - ' + event.url); } );
+        ref.addEventListener('exit', function(event) { console.log(event.type); } );
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+};
